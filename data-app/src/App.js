093cba5a7270e9d77e687f46023a7467e17042cb
JamieStelling <jamie14.stelling@mail.com>
2023-11-29 15:24:23 +0000

    Adding the api

--- data-app/src/App.js
+++ data-app/src/App.js
@@ -1,7 +1,24 @@
 import './App.css';
+import { useState } from "react";
+import { useEffect } from 'react';
 import Button from './components/button';
 
-function App() {
+const  App = () => {
+  const [weather, WeatherTime] = useState('https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={0b038fc24ee3faefb17b8079969e0a06
+}')
+
+  const getWeather = async() => {
+    const response = await fetch('')
+    const data = await response.json()
+    WeatherTime(data)
+  }
+  useEffect(() => {
+    getWeather()
+  })
+  
+  
+  
+
   <h1>API App</h1>
   return (
     <>
