56aaefb73b20fac519da9422910399255f7a2031
JamieStelling <jamie14.stelling@mail.com>
2023-11-29 14:54:57 +0000

    Adding react content

---
+++ data-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Button from './components/button';
+
+function App() {
+  <h1>API App</h1>
+  return (
+    <>
+    <div className="App">
+      <Button />
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ data-app/src/components/button.js
@@ -0,0 +1,10 @@
+
+
+const Button = () => {
+    return(
+        <button>Click for fun</button>
+    )
+}
+
+export default Button();
+
